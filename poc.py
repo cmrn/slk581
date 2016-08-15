66ad44b94b8c739d53b07324d3aae4e52d05f1dc
Ben McGinnes <ben@adversary.org>
2016-08-15 17:09:53 +1000

    Non-binary names possible now
    * Added intersex names which merge female and male names.
    * Intersex option will be used whenever the last number is not a 1 or
      a 2, for male and female, respectively.
    * This will work with intersex (3) and unstated (9) options in SLKs.

--- poc.py
+++ poc.py
@@ -8,6 +8,8 @@ with open('first-names-male.txt') as f:
 with open('last-names.txt') as f:
     last_names = f.read().splitlines()
 
+intsex_names = female_names + male_names
+
 stdout.write('SLK: ')
 slk = stdin.readline().strip()
 
@@ -16,9 +18,12 @@ slk_first = slk[3:5].replace('2', '.')
 slk_last = slk[0:3].replace('2', '.')
 slk_dob = slk[5:13]
 
-first_names = female_names
 if slk_gender == '1':
     first_names = male_names
+elif slk_gender == '2':
+    first_names = female_names
+else:
+    first_names = intsex_names
 
 first = [name for name in first_names if re.match('.' + slk_first, name) is not None]
 last = [name for name in last_names if re.match('.' + slk_last[0:2] + '.' + slk_last[2], name) is not None]
